ee8a3dc797009d346e23bfc0e48cee9d68118661
supriya-sudo89 <210336supriya@cosmoscollege.edu.np>
2026-05-09 19:00:19 +0545

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,14 @@
+num1 = 10
+num2 =3
+
+
+##calculation
+sum = num1+num2
+diff = num1- num2
+mod = num1% num2
+cube = num1**3
+print(f"total is{sum}")
+print(f"diff is{diff}")
+print(f"mod is{mod}")
+print(f"cube is{cube}")
+
